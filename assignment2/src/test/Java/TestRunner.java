24117dc5dcbbbcb34a16e4ec28ea0f1f8d6cbe5d
Ásgeir Frímannsson <Asgeir@Asgeirs-MacBook-Pro-2.local>
2016-09-29 14:05:40 +0000

    asd

---
+++ assignment2/src/test/Java/TestRunner.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by Asgeir on 28/09/2016.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({TestUserService.class, TestVideoService.class} )
+public class TestRunner {
+}
--- assignment2/src/test/Java/TestUserService.java
+++ assignment2/src/test/Java/TestUserService.java
@@ -1,4 +1,4 @@
-import is.ru.honn.rutube.domain.Video;
+import is.ru.honn.rutube.domain.User;
 import is.ru.honn.rutube.service.UserService;
 import is.ru.honn.rutube.service.ServiceException;
 import is.ru.honn.rutube.service.UserServiceStub;
@@ -11,9 +11,12 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:test.resources.spring.config")
 
 public class TestUserService extends TestCase {
     Logger log = Logger.getLogger(TestUserService.class.getName());
@@ -21,7 +24,24 @@ public class TestUserService extends TestCase {
 
     @Before
     public void setup() {
-        ApplicationContext ctx = new FileSystemXmlApplicationContext("classpath:resources/spring.config");
-        service = new UserServiceStub();
+        ApplicationContext ctx = new FileSystemXmlApplicationContext("classpath:..test.resources.spring.config");
+        User user1 = (User)ctx.getBean("user1");
+        User user2 = (User)ctx.getBean("user2");
+        List<User> list = new ArrayList<User>();
+        list.add(user1);
+        list.add(user2);
+        service = new UserServiceStub(list);
+    }
+
+    @Test
+    public void testAddUser() {
+        User testUser = new User(12345, "Ásgeir", "Frímannsson", "asgeirf14@ru.is", "GeiriFriCool17", "1993-08-08");
+        int userSize = -1;
+        try {
+            userSize = service.addUser(testUser);
+        } catch(Exception e) {
+            assertEquals(3, userSize);
+        }
+        assertEquals(3, userSize);
     }
 }
